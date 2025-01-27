d2800bdac1c3d1aac5e6e16f3a9a7bebf41b49fa
Shivani Saroj <shivanis.ug22.cs@nitp.ac.in>
2025-01-28 00:40:19 +0530

    Context in React

---
+++ Mini-Project/src/context/UserContext.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const UserContext= React.createContext()
+
+export default UserContext;
+
+{/* <userContext>
+    <Login/>
+    <Card/>
+    <Data/>
+
+    these all element will get access of the userContext
+</userContext> */}
+
---
+++ Project/My-Project/src/App.jsx
@@ -0,0 +1,16 @@
+
+import { useState } from 'react'
+import './App.css'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+  <h1 className='text-white-500'>abc</h1>
+    </>
+  )
+}
+
+export default App
